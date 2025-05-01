abc2b4bd2113c917d301e0d32f751d398fcf34e2
Mihail Sevdin <misosevdin@gmail.com>
2025-05-01 15:56:00 +0200

    pushtai

--- KIT/src/Datum.java
+++ KIT/src/Datum.java
@@ -1,4 +1,3 @@
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +8,7 @@ public class Datum {
     String jahr;
 
     int indexM;
-    int c;
+    int cKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk;
     int y;
 
 
@@ -72,7 +71,7 @@ public class Datum {
         char[] jahrChars = realJahr.toCharArray();
 
         String temporaryC = "" + jahrChars[0] + jahrChars[1];
-        c = Integer.parseInt(temporaryC);
+        cKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk = Integer.parseInt(temporaryC);
 
         String temporaryY = "" + jahrChars[2] + jahrChars[3];
         y = Integer.parseInt(temporaryY);
@@ -80,8 +79,8 @@ public class Datum {
 
     }
 
-    int getC(){
-        return c;
+    int getcKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk(){
+        return cKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk;
     }
 
     int getY(){
@@ -100,8 +99,8 @@ public class Datum {
 
         int testMid = (int) Math.floor(2.6 * m - 0.2);
         int yDivide = (int) Math.floor(y / 4);
-        int cDivide =(int) Math.floor(c / 4);
-        int preW = (Integer.parseInt(tag) + testMid + y  + yDivide + cDivide - 2 * c);
+        int cDivide =(int) Math.floor(cKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk / 4);
+        int preW = (Integer.parseInt(tag) + testMid + y  + yDivide + cDivide - 2 * cKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKk);
 
         w = mod(preW,7);
 
